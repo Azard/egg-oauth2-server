19c080cb452649bc95cdd6a4296743ea56d5c192
Azard <330815461@qq.com>
2017-05-24 14:53:17 +0800

    #3 release 1.1.0: plugin name use oauth2Server, add some start error handle

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ module.exports = app => {
   app.coreLogger.info('[egg-oauth2-server] egg-oauth2-server begin start');
   const start = Date.now();
   app.config.oauth2Server.model = app.loader.loadFile(path.join(app.config.baseDir, 'app/extend/oauth.js'));
-  app.oauth = oauth2server(app.config.oauth2Server);
+  if (app.config.oauth2Server.model === null) {
+    app.coreLogger.error('[egg-oauth2-server] not find app/extend/oauth.js, egg-oauth2-server start fail');
+    return;
+  }
+  try {
+    app.oauth = oauth2server(app.config.oauth2Server);
+  } catch (e) {
+    app.coreLogger.error('[egg-oauth2-server] start fail, %s', e);
+    return;
+  }
   app.coreLogger.info('[egg-oauth2-server] egg-oauth2-server started use %d ms', Date.now() - start);
 };
