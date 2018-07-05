b63f7c16c8ba56fc6984abefc57848ed7a3c30e3
zemzheng <zemzheng@tencent.com>
2018-07-05 16:53:13 +0800

    fix #31 允许使用 app/extend/oauth.ts

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ module.exports = app => {
   const start = Date.now();
 
   const config = app.config.oAuth2Server;
-  const model = app.loader.loadFile(path.join(app.config.baseDir, 'app/extend/oauth.js'));
+  const model = app.loader.loadFile(path.join(app.config.baseDir, 'app/extend/oauth.js'))
+        // @see https://github.com/Azard/egg-oauth2-server/issues/31
+        || app.loader.loadFile(path.join(app.config.baseDir, 'app/extend/oauth.ts'));
 
   if (model === null) {
     app.coreLogger.error('[egg-oauth2-server] not find app/extend/oauth.js, egg-oauth2-server start fail');
